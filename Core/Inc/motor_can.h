91f929e3cc57a1e039c5d22e7be67d3df91c8707
pear-pro <2925741372@qq.com>
2025-12-16 22:50:48 +0800

    重构代码：更新角度归一化宏定义，添加电机信息数组声明，优化电机速度设置函数，简化PID计算逻辑

--- Core/Inc/motor_can.h
+++ Core/Inc/motor_can.h
@@ -10,8 +10,11 @@
 
 
 */
-#define NORMALIZE_ANGLE180(angle) angle = ((angle) > 180) ? ((angle) - 360) : (((angle) < -180) ? (angle) + 360 : angle)
-//角度归一化
+#define NORMALIZE_ANGLE180(angle) angle = ((angle) > 180) ? ((angle) - 360) : (((angle) < -180) ? (angle) + 360 : angle)//角度归一化
+#define MotorCount 4
+extern motor_info_t C620[MotorCount];
+extern motor_info_t C6xx[MotorCount];
+
 typedef struct{
 	int16_t Voltage;//电压值
 	uint16_t Angle;//机械角度
@@ -38,6 +41,8 @@ typedef struct{
 
 void can_filter_init(void);
 void Set_voltage(CAN_HandleTypeDef* hcan,int16_t vlotage[]);
+void Set_voltagec1(CAN_HandleTypeDef* hcan,int16_t vlotage[]);
+void Set_voltagec2(CAN_HandleTypeDef* hcan,int16_t vlotage[]);
 
 #endif
 
