db4a7187e2b07e3c1ed15dafc7e3abb511cbe176
pear-pro <2925741372@qq.com>
2025-12-16 17:06:36 +0800

    1

---
+++ Core/Inc/includes.h
@@ -0,0 +1,14 @@
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
+
+#include <stdint.h>
+#include <math.h>
+#include "pid.h"
+
+
+#include "main.h" 
+#include "motor_can.h"
+#include "pid_tim.h"
+
+#endif
---
+++ Core/Inc/motor_can.h
@@ -0,0 +1,43 @@
+
+#ifndef __MOTOR_CAN_H
+#define __MOTOR_CAN_H
+
+#include "main.h"
+#include "can.h"
+#include "pid.h"
+/*
+
+
+
+*/
+#define NORMALIZE_ANGLE180(angle) angle = ((angle) > 180) ? ((angle) - 360) : (((angle) < -180) ? (angle) + 360 : angle)
+//角度归一化
+typedef struct{
+	int16_t Voltage;//电压值
+	uint16_t Angle;//机械角度
+	int16_t Speed;//转速
+	int16_t Torque;//实际扭矩
+	uint8_t Temp;//温度
+	
+}RxMsg_t;
+
+typedef struct{
+	pid_t 				Speed_pid;
+	pid_t 				Angel_pid;
+	
+	//控制角度的参数
+	double 				lastRead;
+	double 				Real;//相对零点转了多少度
+	uint16_t 			Zero;//上电后的第一个位置做为零点
+	float				encoderAngle;//经过处理的电机角度
+	
+	RxMsg_t 			Rxmsg;
+}motor_info_t;
+
+
+
+void can_filter_init(void);
+void Set_voltage(CAN_HandleTypeDef* hcan,int16_t vlotage1,int16_t vlotage2,int16_t vlotage3,int16_t vlotage4);
+
+#endif
+
